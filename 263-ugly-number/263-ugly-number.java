e1ce23c890dc9c6574caeae1075cc4e17e9a5fc4
youshamahmood <youshamahmood96@gmail.com>
2022-05-08 12:33:28 +0600

    Time: 2 ms (49.01%), Space: 41.3 MB (50.78%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        if(n%2==0){
+           while(n%2==0) n = n/2;
+        }
+        if(n%3==0){
+           while(n%3==0) n = n/3;
+        }
+        if(n%5==0){
+           while(n%5==0) n = n/5;
+        }
+        return n==1;
+    }
+}
