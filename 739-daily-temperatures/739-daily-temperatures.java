b0708c65ae52fc8c5987abae2096368434745129
youshamahmood <youshamahmood96@gmail.com>
2022-05-09 07:41:58 +0600

    Time: 30 ms (82.21%), Space: 133.5 MB (23.40%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] dailyTemperatures(int[] t) {
         int[] res = new int[t.length];
-        Stack<Integer> s = new Stack<>();
+        Deque<Integer> s = new ArrayDeque<>();
         for(int i=0;i<t.length;i++){
             while(!s.isEmpty() && t[s.peek()]<t[i]){
                 int ans = s.pop();
