9f62cc98daf15694ba5bd42fc4b9abe8f6c22458
youshamahmood <youshamahmood96@gmail.com>
2022-07-18 14:32:58 +0900

    Time: 104 ms (45.59%), Space: 45.9 MB (10.22%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -10,7 +10,49 @@
  * @param {TreeNode} root
  * @return {number}
  */
+class Q{
+    constructor(){
+        this.q = new Map();
+        this.idx = 0;
+        this.curr = 0;
+    }
+    in(value){
+        this.q.set(this.idx, value);
+        this.idx++;
+    }
+    out(){
+        let curr = this.q.get(this.curr);
+        this.q.delete(this.curr);
+        this.curr++;
+        return curr;
+    }
+    isEmpty(){
+        return this.q.size === 0;
+    }
+    size(){
+        return this.q.size;
+    }
+    peek(){
+        return this.q.get(this.curr)
+    }
+}
 var maxDepth = function(root) {
     if(!root) return 0;
-    return Math.max(maxDepth(root.left),maxDepth(root.right))+1
+    let count = 0;
+    let q = new Q();
+    q.in(root);
+    while(!q.isEmpty()){
+        let n = q.size();
+        for(let i=0;i<n;i++){
+            if(q.peek().left !== null){
+                q.in(q.peek().left)
+            } 
+            if(q.peek().right !== null){
+                q.in(q.peek().right)
+            } 
+            q.out()
+        }
+        count++
+    }
+    return count
 };
