13dcf1cfec2afda895c17a1ecf71db165cd76372
youshamahmood <youshamahmood96@gmail.com>
2022-07-18 23:02:51 +0900

    Time: 121 ms (33.93%), Space: 48.8 MB (25.30%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -11,48 +11,14 @@
  * @param {number} k
  * @return {number}
  */
-class Q{
-    constructor(){
-        this.q = new Map();
-        this.idx = 0;
-        this.curr = 0;
-    }
-    in(value){
-        this.q.set(this.idx, value);
-        this.idx++;
-    }
-    out(){
-        let curr = this.q.get(this.curr);
-        this.q.delete(this.curr);
-        this.curr++;
-        return curr;
-    }
-    isEmpty(){
-        return this.q.size === 0;
-    }
-    peek(){
-        if(this.q.has(this.curr)){
-            return this.q.get(this.curr);
-        }
-        else{
-            return null;
-        }
-    }
-}
 var kthSmallest = function(root, k) {
-    let q = new Q()
-    q.in(root)
-    let visited = []
-    while(!q.isEmpty()){
-        let curr = q.out()
-        visited.push(curr.val)
-        if(curr.left){
-            q.in(curr.left)
-        }
-        if(curr.right){
-            q.in(curr.right)
-        }
-    }
-    visited.sort((a,b)=>a-b)
-    return visited[k-1]
+   let arr = []
+   let inOrder = (root) => {
+       if(!root) return;
+       if(root.left) inOrder(root.left)
+       arr.push(root.val)
+       if(root.right) inOrder(root.right)
+   }
+   inOrder(root)
+   return arr[k-1]
 };
