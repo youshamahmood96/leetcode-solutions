3c3cd4a8dbdd072cabbc47ad169cbd3f8ac07e4d
youshamahmood <youshamahmood96@gmail.com>
2022-07-20 22:14:28 +0900

    Time: 157 ms (5.10%), Space: 45.3 MB (44.78%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -10,43 +10,16 @@
  * @param {TreeNode} root
  * @return {number}
  */
-class Q{
-    constructor(){
-        this.q = new Map();
-        this.idx = 0;
-        this.curr = 0;
-    }
-    in(value){
-        this.q.set(this.idx, value);
-        this.idx++;
-    }
-    out(){
-        let curr = this.q.get(this.curr);
-        this.q.delete(this.curr);
-        this.curr++;
-        return curr;
-    }
-    isEmpty(){
-        return this.q.size === 0;
-    }
-    size(){
-        return this.q.size;
-    }
-    peek(){
-        return this.q.get(this.curr)
-    }
-}
 var maxDepth = function(root) {
-    if(!root) return 0;
+    if(!root) return 0
+    let q = [root]
     let count = 0;
-    let q = new Q();
-    q.in(root);
-    while(!q.isEmpty()){
-        let n = q.size();
+    while(q.length>0){
+        let n = q.length
         for(let i=0;i<n;i++){
-            if(q.peek().left !== null) q.in(q.peek().left)
-            if(q.peek().right !== null) q.in(q.peek().right)
-            q.out()
+            let curr = q.shift()
+            if(curr.left)q.push(curr.left)
+            if(curr.right)q.push(curr.right)
         }
         count++
     }
