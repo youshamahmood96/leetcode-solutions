f47d432d71d86b0481edfd8caf806c6e8e6e61ba
youshamahmood <youshamahmood96@gmail.com>
2022-07-22 16:15:57 +0900

    Time: 111 ms (22.48%), Space: 43.9 MB (48.04%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -15,15 +15,15 @@ var rightSideView = function(root) {
     let q = [root]
     let res = []
     while(q.length>0){
-        let arr = []
         let n = q.length
         for(let i=0;i<n;i++){
             let curr = q.shift()
-            arr.push(curr.val)
+            if(i==n-1){
+                res.push(curr.val)
+            }
             if(curr.left) q.push(curr.left)
             if(curr.right) q.push(curr.right)
         }
-        res.push(arr[arr.length-1])
     }
     return res
 };
