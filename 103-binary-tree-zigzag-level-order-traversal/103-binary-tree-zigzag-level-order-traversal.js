381333dac91de9a78746b736d65b6b67286c3c79
youshamahmood <youshamahmood96@gmail.com>
2022-07-23 20:05:26 +0900

    Time: 115 ms (18.36%), Space: 43.9 MB (62.64%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -18,14 +18,17 @@ var zigzagLevelOrder = function(root) {
     while(q.length>0){
         let n = q.length
         let temp = []
-        console.log(lv)
         for(let i=0;i<n;i++){
             let curr = q.shift()
             if(curr.left) q.push(curr.left)
             if(curr.right) q.push(curr.right)
-            temp.push(curr.val)
+            if(lv%2===0){
+                temp.unshift(curr.val)
+            }
+            else{
+                temp.push(curr.val)
+            }
         }
-        if(lv%2===0) temp.reverse()
         res.push(temp)
         lv++
     }
